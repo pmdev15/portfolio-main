96c11dad63813952bb9326d8fdc83180f943625f
pullak404 <pullak404@gmail.com>
2025-11-08 16:16:00 +0530

    requirement.txt

--- static/js/main.js
+++ static/js/main.js
@@ -8,3 +8,12 @@ setInterval(function() {            // setInterval makes it run repeatedly
         .innerHTML = title[i++];    // get the item and increment i to move to the next
     if (i == title.length) i = 0;   // reset to first element if you've reached the end
 }, 1000);  
+
+function myFunction() {
+  var x = document.getElementById("links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
